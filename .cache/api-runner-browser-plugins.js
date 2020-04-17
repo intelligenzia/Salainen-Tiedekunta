d2e3e556d9092e536eaa51af8c596a692b7f918a
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2020-04-17 20:20:44 +0300

    Remove babel

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Salainen Tiedekunta","short_name":"Salainen Tiedekunta","start_url":"/","background_color":"#fff","theme_color":"#373737","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/api-runner-ssr.js
@@ -0,0 +1,55 @@
+var plugins = [{
+      plugin: require('/Users/perttu/Projects/Salainen-Tiedekunta/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/perttu/Projects/Salainen-Tiedekunta/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/perttu/Projects/Salainen-Tiedekunta/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      options: {"plugins":[],"name":"Salainen Tiedekunta","short_name":"Salainen Tiedekunta","start_url":"/","background_color":"#fff","theme_color":"#373737","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
+    },{
+      plugin: require('/Users/perttu/Projects/Salainen-Tiedekunta/node_modules/gatsby-plugin-prefetch-google-fonts/gatsby-ssr'),
+      options: {"plugins":[],"fonts":[{"family":"Alfa Slab One"},{"family":"Domine"}]},
+    }]
+// During bootstrap, we write requires at top of this file which looks like:
+// var plugins = [
+//   {
+//     plugin: require("/path/to/plugin1/gatsby-ssr.js"),
+//     options: { ... },
+//   },
+//   {
+//     plugin: require("/path/to/plugin2/gatsby-ssr.js"),
+//     options: { ... },
+//   },
+// ]
+
+const apis = require(`./api-ssr-docs`)
+
+// Run the specified API in any plugins that have implemented it
+module.exports = (api, args, defaultReturn, argTransform) => {
+  if (!apis[api]) {
+    console.log(`This API doesn't exist`, api)
+  }
+
+  // Run each plugin in series.
+  // eslint-disable-next-line no-undef
+  let results = plugins.map(plugin => {
+    if (!plugin.plugin[api]) {
+      return undefined
+    }
+    const result = plugin.plugin[api](args, plugin.options)
+    if (result && argTransform) {
+      args = argTransform({ args, result })
+    }
+    return result
+  })
+
+  // Filter out undefined results.
+  results = results.filter(result => typeof result !== `undefined`)
+
+  if (results.length > 0) {
+    return results
+  } else {
+    return [defaultReturn]
+  }
+}
