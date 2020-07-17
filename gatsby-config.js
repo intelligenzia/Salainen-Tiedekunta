5d6dd48cd0244a39acf6688dd917c5aa29f07a00
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-07-17 09:18:00 +0300

    Fix ignore and remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   siteMetadata: {
@@ -34,7 +36,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#373737`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
